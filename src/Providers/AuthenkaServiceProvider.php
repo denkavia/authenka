edd67519046ec60d658207ce76d34cd60061a152
denkavia <denkavia.contactme@proton.me>
2026-05-13 21:51:56 +0700

    chore: add empty main service provider

---
+++ src/Providers/AuthenkaServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Denkavia\Authenka\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthenkaServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
